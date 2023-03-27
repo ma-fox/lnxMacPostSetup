161d7aa3fb3e89ab7895b2178a64bcea339c369b
Marco Fox <marco.a.fox@gmail.com>
2023-03-27 13:19:05 +0200

    added lnx os dev tools

--- 02_developer_tools.sh
+++ 02_developer_tools.sh
@@ -21,5 +21,17 @@ if [[ $(uname -s) == "Darwin" ]]; then
 else
   # Ubuntu or other Debian-based Linux distros
   echo "Installing developer tools for Linux..."
-
+  # Install Visual Studio Code
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  rm -f packages.microsoft.gpg
+  sudo apt-get update
+  sudo apt-get install -y code
+  # Install useful VS Code extensions
+  code --install-extension emmanuelbeziat.vscode-great-icons
+  code --install-extension ms-python.python
+  code --install-extension KevinRose.vsc-python-indent
+  code --install-extension ms-python.vscode-pylance
+  code --install-extension ms-toolsai.jupyter
 fi
